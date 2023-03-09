f1f6a5d765523585d719cfe0c9f58e2ec4252690
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-03-08 22:14:02 -0500

    Installed styled-components and its @types

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
