de1c8c183542e963e010b97117ad435ae6e0d205
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-03-08 21:59:10 -0500

    Created theme file

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,5 +1,18 @@
 export default {
     COLORS:{
+        background: '#DEDEDE',
+        brand_green_light: '#E5F0DB',
+        brand_gree_mid: '#CBE4B4',
+        brand_green_dark: '#639339',
+        brand_red_light: '#F4E6E7',
+        brand_red_mid: '#F3BABD',
+        brand_red_dark: '#BF3B44',
+        base_white: '#FFFFFF',
+        base_gray_100:  '#1B1D1E',
+        base_gray_200: '#333638',
+        base_gray_500: '#DDDEDF',
+        base_gray_600: '#EFF0F0',
+        base_gray_700: '#FAFAFA'
 
     },
     FONT_FAMILY:{
