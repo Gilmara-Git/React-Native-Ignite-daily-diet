00f6dd947ca98a02e135bc313fa2ec6545e92926
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-03-16 08:22:07 -0400

    Added @storage to the babel plugin module resolve and configured it on the babel config file and the typescript config file

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,7 @@ module.exports = function(api) {
             '@components': './src/components',
             '@routes': './src/routes',
             '@themes': './src/themes',
+            '@storage': './src/storage'
           }
         }
       ]
