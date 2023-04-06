b669555340b3d500c609f6b4baafc1b495f8671c
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-04-06 06:54:28 -0400

    refactor: adjusted boolean in SectionList

--- src/components/SectionListComponents/SectionListItem/index.tsx
+++ src/components/SectionListComponents/SectionListItem/index.tsx
@@ -1,21 +1,22 @@
 import { Container, Time, MealName, IndicatorDot,Separator } from './styles';
+import { useTheme } from 'styled-components/native';
 
 type SectionListItem ={
     time: string;
     mealName : string; 
     widthDimensions: number;
-    indicatorColor: string;
+    indicatorColor: boolean;
 }
 
 
 export const SectionListItem = ({ widthDimensions, time, mealName, indicatorColor }:SectionListItem)=>{
-
-    return (
+    const theme = useTheme()
+;    return (
         <Container widthDimensions={widthDimensions}>
             <Time>{time}</Time>
             <Separator />
             <MealName>{mealName}</MealName>
-            <IndicatorDot indicatorColor={indicatorColor}/>
+            <IndicatorDot indicatorColor={indicatorColor ? theme.COLORS.brand_green_mid: theme.COLORS.brand_red_mid}/>
         </Container>
     )
 };
