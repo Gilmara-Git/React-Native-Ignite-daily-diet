fdec9d5031098406a9437044f03d6480d551bdb2
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-04-07 21:40:15 -0400

    refactored

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,4 @@
 import { ModalBackdrop, InteractionBox, Title } from './styles';
-import { MainButton } from '@components/MainButton';
-import { useTheme } from 'styled-components/native';
 import { TouchableOpacityProps } from 'react-native';
 import React from 'react';
 
@@ -12,8 +10,6 @@ type ModalProps = TouchableOpacityProps &{
 }
 
 export const Modal = ({ height, title, children, onCancel, ...rest}: ModalProps)=>{
-    console.log(height)
-    const theme =  useTheme();
 
     return (
 
